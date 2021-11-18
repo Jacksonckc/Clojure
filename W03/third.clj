754908371f40e44bf3627916d9839319d6762925
Jacksonckc <jackson858216047@gmail.com>
2021-11-18 16:25:05 -0700

    1

--- W03/third.clj
+++ W03/third.clj
@@ -6,6 +6,8 @@
        (println a '= b (list (quot a b)) '+ (rem a b))
        (recur b (mod a b)))))
        
+
+       
 ;; Takes only 1 input, generates a random number which is smaller than the input,
 ;; then finds the gcd for those two numbers.
 (defn gcd
@@ -19,7 +21,8 @@
 
 
 
-;; 
+;; This function receives one input and use the input to call gcd function,
+;; loop through the gcd function 20 times to ensure a number is being tested many times to avoid inaccuracy.
 (defn prime_pretender_test[b]
   (loop [x 20]
     (when (>= x 1)
